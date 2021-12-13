b3199c34c9a64dcd766c03507bd65af8eb3e6637
Pedro Felipe <felipexexeu02@gmail.com>
2021-12-13 17:35:18 -0300

    html-Avançado: canvas animação

--- html-Avancado/canvas/scripts/script.js
+++ html-Avancado/canvas/scripts/script.js
@@ -22,28 +22,22 @@ ctx.stroke();
 
 ctx.clearRect(40, 420, 400, 20);
 
-// Path
-ctx.beginPath();
-ctx.strokeStyle = "black";
-ctx.fillStyle = "green";
-ctx.moveTo(25, 75);
-ctx.lineTo(200, 250);
-ctx.lineTo(25, 250);
-ctx.closePath();
-ctx.fill();
-ctx.stroke();
+let circulo = {
+    x: 250,
+    y: 250,
+    raio: 100,
+    inicio: 0,
+    fim: 2 * Math.PI,
+}
 
-// Circulo
-ctx.beginPath();
-ctx.strokeStyle = "black";
-ctx.fillStyle = "red";
+function drawCirculo(c) {
+    ctx.beginPath();
+    ctx.strokeStyle = "red";
+    ctx.fillStyle = "blue";
 
-let x = 250;
-let y = 250;
-let raio = 50;
-let inicio = 0;
-let fim = 2 * Math.PI;
+    ctx.arc(c.x, c.y, c.raio, c.inicio, c.fim);
+    ctx.fill();
+    ctx.stroke();
+}
 
-ctx.arc(x, y, raio, inicio, fim);
-ctx.fill();
-ctx.stroke();
+drawCirculo(circulo);
