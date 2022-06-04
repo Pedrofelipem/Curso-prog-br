be141e141e7fe4eb75ba666814cc8e43b90f2586
Pedrofelipem <felipexexeu02@gmail.com>
2022-06-04 16:20:09 -0300

    javascript-Avançado:

--- javaScript-Avancado/script/script.js
+++ javaScript-Avancado/script/script.js
@@ -64,7 +64,7 @@ inserirUsuario("Edvan")
     .catch((error) => {
         console.log(error);
     })
-*/
+
 let usuarios = ["Pedro", "Felipe", "Maria"]
 
 function inserirUsuario(nome){
@@ -95,5 +95,21 @@ async function executar(){
 }
 
 executar();
-
+*/
     
+function novosAluno(nome, idade){
+    return {nome, idade}
+}
+
+let alunos = [
+  novosAluno("Pedro", 22),
+  novosAluno("Gabriel", 31),
+  novosAluno("Henrique", 29),
+  novosAluno("Paula", 45)
+]
+
+function menoresde30(aluno){
+    return aluno.idade < 30
+}
+
+console.log(alunos.filter(menoresde30));
